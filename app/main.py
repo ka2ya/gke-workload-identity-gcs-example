b075fbbe6a3eb3bb58c6ceda5bb24e8c52e7eb97
sximada <50688746+sximada@users.noreply.github.com>
2020-04-21 15:34:44 +0900

    Update app.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import os
 
 from google.auth.credentials import AnonymousCredentials
 from google.cloud import storage
-from google.cloud.storage.blob import Blob
 
 
 BUCKET_NAME = os.environ["BUCKET_NAME"]
@@ -12,26 +11,8 @@ data = "OK!!"
 credentials = AnonymousCredentials()
 
 client = storage.Client(project="default", credentials=credentials)
-bucket = client.bucket(BUCKET_NAME,)
-bucket.create()
+bucket = client.bucket(BUCKET_NAME)
 blob = bucket.blob('test.txt')
 blob.upload_from_string(data)
-blob2 = bucket.get_blob('test.txt', generation=0)
-print(blob2._get_download_url())
-
-
-# blob2 = Blob(
-#     bucket=bucket,
-#     name='test.txt',
-#     encryption_key=None,
-#     generation=0,
-# )
-# blob2.reload(client=client, timeout=10)
-# blob2._properties['Generation'] = 1
-# print(bucket.get_blob('TESTFILE').download_as_string())
-# bucket = client.get_bucket(BUCKET_NAME)
-# blob = bucket.get_blob('TESTFILE')
-# resp = blob.upload_from_string(data)
-# blob2 = bucket.blob('TESTFILE')
-# resp_data = blob2.download_as_string()
-# print(resp_data)
+blob2 = bucket.get_blob('test.txt')
+print(blob2)
